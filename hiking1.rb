64f83bad61c559ce9f9ff1b653399ef3e87cb0b4
PaulaTTS <paula@techtalentsouth.com>
2015-05-11 22:27:55 +0000

    hiking1 updates

--- hiking1.rb
+++ hiking1.rb
@@ -1,5 +1,14 @@
 todays_temperature = 75
 
+puts "what's the temperature?"
+todays_temperature = gets.chomp.to_i
+
 if todays_temperature > 50
   puts "Let's go hiking!"
+elsif todays_temperature < 32
+  puts "it's cold!!!"
+elsif todays_temperature == 42
+  puts "that's the answer to life, the universe, and everything!"
+else 
+  puts "it's not warm enough to hike, but not that cold."
 end
