5f131ff8923fe76ef9153862fdce778c1550c1bb
PaulaTTS <paula@techtalentsouth.com>
2015-05-19 20:59:56 +0000

    weeks.rb, just for fun

--- weeks.rb
+++ weeks.rb
@@ -1,5 +1,17 @@
-week = ["1st","2nd","3rd","4th","5th","6th","7th","8th"]
-puts "Want to know what week it is?  Hit enter!"
+done = false
 
-gets
-puts "It's the " + week[3] + " week of class!"
+while done == false
+
+  week = ["1st","2nd","3rd","4th","5th","6th","7th","8th"]
+  puts "Want to know what week it is?  Hit enter!"
+
+  gets
+  puts "It's the " + week[3] + " week of class!"
+
+  puts "want me to ask you again?  Enter 'OK' if you've had enough"
+  donetext = gets.chomp.upcase
+  if donetext == "OK"
+    done = true
+  end
+ 
+end
